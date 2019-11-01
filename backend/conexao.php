661a9e195f4e591b14d51a8ba0ef6787e778a546
Jose Afranio Volpato Junior <jose.avjunior@sp.senac.br>
2019-10-31 22:26:36 -0300

    first commit

---
+++ backend/conexao.php
@@ -0,0 +1,15 @@
+<?php
+//Conexao ao banco de dados.
+$local = "127.0.0.1:3306";
+$user = "root";
+$passwd = "";
+$bd = "projetophp";
+
+$link = mysqli_connect("$local","$user","$passwd","$bd");
+
+if (!$link) {
+        echo "Não conectado ao banco"; exit();
+    }
+    
+   // echo "Banco conectado com sucesso";
+?>
